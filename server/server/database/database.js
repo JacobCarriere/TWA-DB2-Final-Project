6413544ba294e6dc37bbff352afdb4511a9861c4
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2024-05-02 14:43:20 -0400

    created connection to the database

---
+++ server/server/database/database.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+// Connection URL
+const databaseName = '' // change the database name. Ask Joshua
+const url = `${process.env.DB_HOST}/${databaseName}`; // edit this into this later: `${process.env.DB_HOST}/${databaseName}`;
+
+async function connect() {
+    // Use connect method to connect to the server
+    await mongoose.connect(url);
+    console.log('Connected successfully to MongoDB database');
+
+    return 'done.';
+}
+
+// We export the connect method and the collection reference
+module.exports = {
+    connect
+}
