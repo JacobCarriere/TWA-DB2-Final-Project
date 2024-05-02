0e2266aa88e86409aecda0c936e24ed9e8c9de48
JacobCarriere <jacob2000@live.com>
2024-05-02 14:50:59 -0400

    client folder added

---
+++ client/src/home.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+
+
+function Home() {
+  
+  return (
+      <div style={{ textAlign: 'center'}}>
+        <h1>Header 1</h1>
+        <h2>Header 2</h2>
+      </div>
+  );
+}
+
+export default Home;
