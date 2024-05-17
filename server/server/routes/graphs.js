26afc7857c57162120792793859618a75f6d6eee
Murtaza Haidary <LE1983326@crc-lennox.qc.ca>
2024-05-17 17:32:05 -0400

    working on the third graph

--- server/server/routes/graphs.js
+++ server/server/routes/graphs.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // ijmporting the jwt token
 const jwt = require('jsonwebtoken');
 
-
+// this router is for the prototype
 router.get('/python/:param1', async (req, res) => {
     // run the Python file
     const python = spawn('python', ['scripts/fossilconsumption.py', req.params.param1]);
@@ -90,9 +90,8 @@ router.get('/firstGraph', async (req, res) => {
 })
 
 
-
 // generate the pie graph based on the json body.
-router.get('/pieChart', async function (req, res) {
+router.post('/pieChart', async function (req, res) {
     const { year, country, graphType } = req.body;
 
     // fossiconsumption = 1, sustainconsumption.py = 2, emissionperiod.py = 3
@@ -143,4 +142,109 @@ router.get('/pieChart', async function (req, res) {
     })
 })
 
+// this route should generate a bar graph
+router.post('/', async function (req, res) {
+    // get the data from the request body
+    const [  ] = req.body;
+
+    // fossiconsumption = 1, sustainconsumption.py = 2, emissionperiod.py = 3
+    // create a map for the scripts.
+    // maybe join.path here?
+    const scriptPaths = {
+        '1': 'scripts/fossilconsumption.py',
+        '2': 'scripts/sustainconsumption.py',
+        '3': 'scripts/emissionperiod.py'
+    }
+
+    // make sure graph is one of in the map.
+    if (!(graphType in scriptPaths)) {
+        return res.status(400).send("You've entered the wrong input for the graphType. Please, choose between 1 and 3")
+    }
+
+    // make sure country is an array
+    const countryArray = Array.isArray(country) ? country : [country];
+
+    // run the python script
+    const python = spawn('python', [scriptPaths[graphType], year, ...countryArray]);
+    console.log('year: ', year);
+    console.log('graphType: ', graphType);
+    console.log('country: ', country);
+
+    // listen to the python sript, and await result.
+    python.on('close', (code) => {
+        console.log('code: ', code);
+        if (code === 0) {
+            // Read the generated image file
+            const imagePath = path.join(__dirname,`../image/sustainconsumption.png`);
+                // Read the image
+                const imageBuffer = fs.readFileSync(imagePath);
+                console.log('imagePath: ', imagePath);
+
+                // Set response headers
+                res.set({
+                    'Content-Type': 'image/png',
+                    'Content-Length': imageBuffer.length
+                });
+
+                // Send the image to response body
+                res.end(imageBuffer);
+        } else {
+            // if there's problem with generating image
+            res.status(500).send('Error: Failed to show the image');
+        }
+    })
+})
+
+// the following is the last graph
+router.post('/', async function (req, res) {
+    // get the data from the request body
+    const { } = req.body;
+
+    // fossiconsumption = 1, sustainconsumption.py = 2, emissionperiod.py = 3
+    // create a map for the scripts.
+    // maybe join.path here?
+    const scriptPaths = {
+        '1': 'scripts/fossilconsumption.py',
+        '2': 'scripts/sustainconsumption.py',
+        '3': 'scripts/emissionperiod.py'
+    }
+
+    // make sure graph is one of in the map.
+    if (!(graphType in scriptPaths)) {
+        return res.status(400).send("You've entered the wrong input for the graphType. Please, choose between 1 and 3")
+    }
+
+    // make sure country is an array
+    const countryArray = Array.isArray(country) ? country : [country];
+
+    // run the python script
+    const python = spawn('python', [scriptPaths[graphType], year, ...countryArray]);
+    console.log('year: ', year);
+    console.log('graphType: ', graphType);
+    console.log('country: ', country);
+
+    // listen to the python sript, and await result.
+    python.on('close', (code) => {
+        console.log('code: ', code);
+        if (code === 0) {
+            // Read the generated image file
+            const imagePath = path.join(__dirname,`../image/sustainconsumption.png`);
+                // Read the image
+                const imageBuffer = fs.readFileSync(imagePath);
+                console.log('imagePath: ', imagePath);
+
+                // Set response headers
+                res.set({
+                    'Content-Type': 'image/png',
+                    'Content-Length': imageBuffer.length
+                });
+
+                // Send the image to response body
+                res.end(imageBuffer);
+        } else {
+            // if there's problem with generating image
+            res.status(500).send('Error: Failed to show the image');
+        }
+    })
+})
 module.exports = router;
