06fde61f153865e0e552adcc32858c0461424aa5
JLac <le2056237"crcmail.com>
2024-05-18 22:15:08 -0400

    Fourth Graph Complete

---
+++ server/server/scripts/energydemand.py
@@ -0,0 +1,43 @@
+import sys
+import pymongo
+import matplotlib.pyplot as plt
+import pandas as pd
+
+client = pymongo.MongoClient('mongodb+srv://joshlacroix:NByewvwMRgR8hM0C@movieappdata.dhsk4vr.mongodb.net/')
+db = client['energy_consumption']
+collection = db['country_energy']
+
+country = sys.argv[1]
+yr = sys.argv[2]
+yr_min = int(yr) - 10
+
+electricity_demand = []
+electricity_generation = []
+year_range = range(yr_min, int(yr))
+
+for year in year_range:
+    data = collection.find_one({'country': country, 'year': year})
+    if data:
+        electricity_demand.append(data['electricity_demand'])
+        electricity_generation.append(data['electricity_generation'])
+    else:
+        electricity_demand.append(None)
+        electricity_generation.append(None)
+
+df = pd.DataFrame({
+    'year': year_range,
+    'electricity_demand': electricity_demand,
+    'electricity_generation': electricity_generation
+})
+
+plt.figure(figsize=(10, 6))
+plt.plot(df['year'], df['electricity_demand'], label='Electricity Demand')
+plt.plot(df['year'], df['electricity_generation'], label='Electricity Generation')
+
+plt.title(f'Energy Supply and Demand Over Time in {country}')
+plt.xlabel('Year')
+plt.xticks(df['year'])
+plt.ylabel('Energy')
+plt.legend()
+plt.savefig("energydemand.png")
+plt.show()
